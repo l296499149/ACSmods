7fb7c15fc3938bcee0445bf6db0ab5398812bf8e
l296499149 <45302254+l296499149@users.noreply.github.com>
2026-03-11 14:09:47 +0800

    Add files via upload

---
+++ 03.buhuizhaohuo/Scripts/NOFIRE.lua
@@ -0,0 +1,14 @@
+﻿local NOFIRE = GameMain:GetMod("NOFIRE");
+local time = 0;
+local flag = 0;
+
+function NOFIRE:OnStep(dt)
+if flag == 0 then
+time = time + dt;
+if time >= 10  then
+flag = 1;
+CS.GameMain.Instance.NoBurning = true
+end
+end
+end
+
